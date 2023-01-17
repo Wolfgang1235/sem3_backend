01bd95e593ec18fa0179a25a713c4d351399748d
Andreas Fritzbøger <andreasfritzboger@live.dk>
2023-01-17 09:39:48 +0100

    Implementing new entites and adding relationship to user entity with tenant

---
+++ src/main/java/entities/House.java
@@ -0,0 +1,75 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "houses")
+public class House {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Size(max = 45)
+    @NotNull
+    @Column(name = "city", nullable = false, length = 45)
+    private String city;
+
+    @NotNull
+    @Column(name = "numberOfRooms", nullable = false)
+    private Integer numberOfRooms;
+
+    @OneToMany(mappedBy = "houses")
+    private Set<Rental> rentals = new LinkedHashSet<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public void setNumberOfRooms(Integer numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public Set<Rental> getRentals() {
+        return rentals;
+    }
+
+    public void setRentals(Set<Rental> rentals) {
+        this.rentals = rentals;
+    }
+
+}
